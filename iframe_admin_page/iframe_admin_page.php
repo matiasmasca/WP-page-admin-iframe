18c3ac8f75570188f55a97b6cc613b2a5d99c488
Matias M <matiasmasca@gmail.com>
2015-12-15 22:09:05 -0300

    limpiando el código, agregando el logo

--- iframe_admin_page/iframe_admin_page.php
+++ iframe_admin_page/iframe_admin_page.php
@@ -9,18 +9,18 @@ Author:       Mascazzini Matias
 Autor URI:    http://about.me/matiasmasca
 License:      MIT
 **/
-
+//Basado en este post: http://www.conasa.es/como-crear-un-plugin-para-wordpress-con-pagina-de-opciones/
 /*
 * Función para añadir una página al menú de administrador de wordpress
 */
 function admin_page_plugin_menu(){
   //Añade una página de menú a wordpress
   add_menu_page('iFram Page Simple Pluggin',     //Título de la página
-          'iFrame Page',            //Título del menú
-          'administrator',              //Rol que puede acceder
-            'admin_page-settings',    //Id de la página de opciones
-            'admin_page_settings', //Función que pinta la página de configuración del plugin
-            'dashicons-admin-generic');         //Icono del menú
+          'iFrame Page',              //Título del menú
+          'administrator',            //Rol que puede acceder
+          'admin_page-settings',      //Id de la página de opciones
+          'admin_page_settings',      //Función que pinta la página de configuración del plugin
+          'dashicons-admin-generic'); //Icono del menú
 }
 add_action('admin_menu','admin_page_plugin_menu');
 
@@ -30,37 +30,14 @@ add_action('admin_menu','admin_page_plugin_menu');
 function admin_page_settings(){
 ?>
   <div class="wrap">
-  <h2>Titulo Pagina</h2>
+  <h2>Titulo Pagina (edita el pluggin para cambiar)</h2>
   <script type="text/javascript">// <![CDATA[
     function preloader(){ document.getElementById("preload").style.display = "none"; document.getElementById("iframe").style.display = "block"; } //preloader window.onload = preloader;
 // ]]></script>
-  <div id="preload"><img src="/media/terlogo1101.png" alt="Terciar" /></div>
+  <div id="preload"><img src="/wp-content/plugins/iframe_admin_page/media/logo.png" alt="www.Terciar.info" /></div>
   <!-- ACA configurar la URL para el iframe -->
   <div id="iframe"><iframe id="app-iframe" src="http://www.comunidadtic.com.ar/" width="100%" height="800" frameborder="0">Si ves este mensaje, significa que tu navegador no tiene soporte para marcos o el mismo está deshabilitado. Puedes acceder a la información mostrada en este marco aquí: <a href="http://www.comunidadtic.com.ar/">Ingresar a la aplicación</a>.</iframe></div>
 </div>
 <?php
-}
-
-/*
-* Función que registra las opciones del formulario en una lista blanca para que puedan ser guardadas
-*/
-function admin_page_settings(){
-  /* register_setting('admin_page-settings-group',
-           'admin_page_url_value',
-           'intval'); */
-}
-add_action('admin_init','admin_page_settings');
-
-/*
-* Función que devuelve el contenido de un post limitado a la longitud configurada en la página de opciones 
-*/
-function admin_page_action($content){
-  /*
-  $url = get_option('admin_page_url_value');
-  return $url  
-  */
-}
-add_filter('the_content','admin_page_action');
-
-//Basado en este post: http://www.conasa.es/como-crear-un-plugin-para-wordpress-con-pagina-de-opciones/
+  }
 ?>
